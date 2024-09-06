97b114f22846c2ccdf1d398012f95f58caa773ed
ignatiusobiorah <iobiorah23@gmail.com>
2024-09-06 11:34:31 +0100

    Modify media player to embed youtube iframe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import KnowUs from "./components/KnowUs";
+import MediaPlayer from "./components/MediaPlayer";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <KnowUs />
+      <MediaPlayer/>
     </div>
   );
 }
